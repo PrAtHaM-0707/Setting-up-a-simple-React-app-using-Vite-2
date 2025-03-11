8f649d55db79c0657d15422af7a65c006aff8b7b
Pratham Mittal <prathamm4402@gmail.com>
2025-03-11 10:22:40 +0530

    Initial commit - React Vite setup

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>My Name: Pratham Mittal</h1>
+      <p>Contact: pratham2005@gmail.com</p>
+    </div>
+  );
+};
+
+export default Home;
